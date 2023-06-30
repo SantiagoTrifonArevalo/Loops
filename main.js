80cabbd55f0cbf4260044a3dc7823e72da767dd2
SantiagoTrifonArevalo <santiagodtrifon@gmail.com>
2023-06-30 20:22:44 +0200

    worked

---
+++ main.js
@@ -0,0 +1,37 @@
+// Ejercicio.1
+for(let i =0; i <= 20; i++) {
+    console.log(i);
+}
+
+// Ejercicio 2
+for(let i = 10; i >= 0; i--) {
+    console.log(i);
+    }
+
+// Ejercicio 3
+let number = 7;
+for (num = 0; num >=10; num++)
+ {
+    console.log(`${number} * ${num}=number*num`);
+    
+}
+
+// Ejercicio 4
+for (let i = 0; i < 11; i++) {
+console.log(`${i} ${i**2} ${i**3}`);    
+}
+   
+
+
+// Ejercicio 5
+for (i = 0; i < 1; i++) {
+    console.log(0+1+2+3+4+5+6+7+8+9+10);}
+// Imprime por consola, una sola vez, la suma de todos los números del 0 al 10.
+
+// Ejercicio 6
+for (let i = 0; i < 1; i++) {
+console.log(0*1*2*3*4*5*6*7*8*9*10);
+    }
+    // Cualquier numero multiplicado por el numero q sea, dará 0
+
+// Haz lo mismo pero con el resultado de multiplicar todos los números del 0 al 10. Una vez estés seguro de que es el resultado correcto, explica en un comentario por qué da ese resultado
